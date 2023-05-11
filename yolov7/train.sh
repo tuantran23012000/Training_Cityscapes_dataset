644866552ede09f31811400790eb0c424dc8a67f
tuantran23012000 <trananhtuan23012000@gmail.com>
2023-05-11 14:00:26 +0700

    first commit

---
+++ yolov7/train.sh
@@ -0,0 +1,1 @@
+python3 train.py --workers 2 --device 'cpu' --batch-size 32 --data data/coco.yaml --img 640 640 --cfg cfg/training/yolov7.yaml --weights '' --name yolov7 --hyp data/hyp.scratch.p5.yaml
